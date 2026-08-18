e7d237989bee291924fce0aec85784151be113d7
Klayton Massango <kmassango1@gmail.com>
2026-08-18 12:05:40 +0200

    check signatures

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::middleware('auth')->group(function(){
   Route::get('/settings/documents/logo',[DocumentSettingsController::class,'logo'])->name('organization.documents.logo');
   Route::get('/settings/documents/header',[DocumentSettingsController::class,'headerBanner'])->name('organization.documents.header');
   Route::resource('events',EventController::class); Route::post('/events/{event}/participants',[ParticipantController::class,'store'])->name('participants.store'); Route::delete('/events/{event}/participants/{participant}',[ParticipantController::class,'destroy'])->name('participants.destroy'); Route::post('/events/{event}/sync-attendance-statuses',[EventController::class,'syncAttendanceStatuses'])->name('events.sync-attendance-statuses');
-  Route::get('/events/{event}/kiosk',[AttendanceController::class,'kiosk'])->name('attendance.kiosk'); Route::post('/events/{event}/attendance',[AttendanceController::class,'store'])->name('attendance.store');
+  Route::get('/events/{event}/kiosk',[AttendanceController::class,'kiosk'])->name('attendance.kiosk'); Route::post('/events/{event}/attendance',[AttendanceController::class,'store'])->name('attendance.store'); Route::get('/attendance-records/{attendanceRecord}/signature',[AttendanceController::class,'signature'])->name('attendance.signature');
   Route::get('/events/{event}/exports/attendance.pdf',[ExportController::class,'attendance'])->name('exports.attendance');
   Route::get('/payment-lists/{paymentList}/export.pdf',[ExportController::class,'payment'])->name('exports.payment');
   Route::get('/payment-lists/{paymentList}',[PaymentController::class,'showList'])->name('payments.lists.show');
